59631e39d66797f9779cfcc5000550a0f4603ff1
XieYunshen <1084314248@qq.com>
2024-04-24 15:38:22 +0800

    add coverage sample

---
+++ item1/cov_sample/test_cov.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int a = 10;
+    if (a > 5) {
+        std::cout << "a is greater than 5" << std::endl;
+    } else {
+        std::cout << "a is not greater than 5" << std::endl;
+    }
+    return 0;
+}
